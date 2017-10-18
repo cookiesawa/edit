9addd5798bbc6c75439322ebcd8022759df02838
cookiesawa <cookiesawa@naver.com>
2017-10-18 13:16:23 +0900

    abcdef

---
+++ Ecar/app/src/main/java/com/example/cooki/ecar/MainActivity.java
@@ -0,0 +1,197 @@
+package com.example.cooki.ecar;
+
+import android.app.FragmentManager;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import android.app.FragmentManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener {
+
+
+    private final int FRAGMENT1 = 1;
+    private final int FRAGMENT2 = 2;
+    private final int FRAGMENT3 = 3;
+    private final int FRAGMENT4 = 4;
+
+    private Button bt_tab1, bt_tab2, bt_tab3, bt_tab4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // 위젯에 대한 참조
+        bt_tab1 = (Button)findViewById(R.id.bt_tab1);
+        bt_tab2 = (Button)findViewById(R.id.bt_tab2);
+        bt_tab3 = (Button)findViewById(R.id.bt_tab3);
+        bt_tab4 = (Button)findViewById(R.id.bt_tab4);
+
+        // 탭 버튼에 대한 리스너 연결
+        bt_tab1.setOnClickListener(this);
+        bt_tab2.setOnClickListener(this);
+        bt_tab3.setOnClickListener(this);
+        bt_tab4.setOnClickListener(this);
+
+        // 임의로 액티비티 호출 시점에 어느 프레그먼트를 프레임레이아웃에 띄울 것인지를 정함
+        callFragment(FRAGMENT1);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_notice) {
+            // Handle the camera action
+        } else if (id == R.id.nav_info) {
+
+        } else if (id == R.id.nav_opensource) {
+
+        } else if (id == R.id.nav_me) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_tab1 :
+                // '버튼1' 클릭 시 '프래그먼트1' 호출
+                callFragment(FRAGMENT1);
+                break;
+
+            case R.id.bt_tab2 :
+                // '버튼2' 클릭 시 '프래그먼트2' 호출
+                callFragment(FRAGMENT2);
+                break;
+            case R.id.bt_tab3 :
+                // '버튼3' 클릭 시 '프래그먼트2' 호출
+                callFragment(FRAGMENT3);
+                break;
+            case R.id.bt_tab4 :
+                // '버튼4' 클릭 시 '프래그먼트2' 호출
+                callFragment(FRAGMENT4);
+                break;
+        }
+    }
+
+    private void callFragment(int frament_no){
+
+        // 프래그먼트 사용을 위해
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (frament_no){
+            case 1:
+                // '프래그먼트1' 호출
+                Fragment1 fragment1 = new Fragment1();
+                transaction.replace(R.id.fragment_container, fragment1);
+                transaction.commit();
+                break;
+
+            case 2:
+                // '프래그먼트2' 호출
+                Fragment2 fragment2 = new Fragment2();
+                transaction.replace(R.id.fragment_container, fragment2);
+                transaction.commit();
+                break;
+            case 3:
+                // '프래그먼트3' 호출
+                Fragment3 fragment3 = new Fragment3();
+                transaction.replace(R.id.fragment_container, fragment3);
+                transaction.commit();
+                break;
+            case 4:
+                // '프래그먼트4' 호출
+                Fragment4 fragment4 = new Fragment4();
+                transaction.replace(R.id.fragment_container, fragment4);
+                transaction.commit();
+                break;
+        }
+
+    }
+
+}
+
